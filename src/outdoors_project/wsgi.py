219507c9695f0bf73ff49dd1e588567420d6e9ed
Ceegan Hale <ceeganhale@gmail.com>
2018-10-06 18:01:11 -0600

    Renamed folder

--- src/outdoors_project/wsgi.py
+++ src/outdoors_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for OutdoorsClub project.
+WSGI config for outdoors_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OutdoorsClub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'outdoors_project.settings')
 
 application = get_wsgi_application()
