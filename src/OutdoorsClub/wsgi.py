a40c4ce44701fa134c75e513d1e9808c216a5c17
Ceegan Hale <ceeganhale@gmail.com>
2018-10-06 18:17:30 -0600

    Revert "Renamed folder"
    This reverts commit 219507c9695f0bf73ff49dd1e588567420d6e9ed.

--- src/OutdoorsClub/wsgi.py
+++ src/OutdoorsClub/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for outdoors_project project.
+WSGI config for OutdoorsClub project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'outdoors_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OutdoorsClub.settings')
 
 application = get_wsgi_application()
