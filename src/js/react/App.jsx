8f27bffde4c9ab56238c45ea2a34f56eeea150ed
Henry Tang <hktang@ualberta.ca>
2018-10-11 14:25:13 -0600

    Gear-Mgmt-04: Added Semantic-ui to package.json and created rudimentary form

--- src/js/react/App.jsx
+++ src/js/react/App.jsx
@@ -5,6 +5,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HelloWorld from "./HelloWorld";
 import GearPage from "./gear/GearPage";
+import AddGearForm from "./gear/forms/AddGearForm";
 
 export default class App extends React.Component {
     render() {
@@ -13,6 +14,7 @@ export default class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HelloWorld} />
                     <Route path='/gear' component={GearPage} />
+                    <Route path'/add-gear' component={AddGearForm} />
                 </Switch>
             </BrowserRouter>
         );
