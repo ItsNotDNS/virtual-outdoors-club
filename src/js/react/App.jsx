08583870928e2ad38ab13982bc81522432f67e21
Alex Dong <adong@ualberta.ca>
2018-10-29 14:22:07 -0600

    Navbar (#184)
    * Main navigation layout implementation
    
    * Fixated to the top during scrolling
    
    * Add reservations directory
    
    * Removed CSS import as per Henry's request

--- src/js/react/App.jsx
+++ src/js/react/App.jsx
@@ -5,6 +5,8 @@
 
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, NavItem, Nav, NavDropdown, MenuItem, Button } from "react-bootstrap";
 import GearPage from "./gear/GearPage";
 import ReservationPage from "./reservation/ReservationPage";
 import RentPage from "./gear/RentPage";
@@ -14,6 +16,46 @@ export default class App extends React.Component {
         return (
             <BrowserRouter>
                 <div className="page-margin">
+                    <Navbar fluid fixedTop >
+                        <Navbar.Header>
+                            <Navbar.Brand>
+                                Administration
+                            </Navbar.Brand>
+                        </Navbar.Header>
+                        <Nav bsStyle="pills">
+                            <LinkContainer to="/gear">
+                                <NavItem eventKey={1}> Gear </NavItem>
+                            </LinkContainer>
+                            <LinkContainer to="/rent">
+                                <NavItem eventKey={2}> Rent </NavItem>
+                            </LinkContainer>
+                            <LinkContainer to="/reservation">
+                                <NavItem eventKey={3}> Reservations </NavItem>
+                            </LinkContainer>
+                            <LinkContainer to="#">
+                                <NavItem disabled eventKey={4}> Users </NavItem>
+                            </LinkContainer>
+                            <NavDropdown eventKey={5} title="Actions" id="nav-dropdown">
+                                <LinkContainer to="#">
+                                    <MenuItem disabled eventKey={5.1}>Enable/Disable Rental System</MenuItem>
+                                </LinkContainer>
+                                <LinkContainer to="#">
+                                    <MenuItem disabled eventKey={5.2}>Statistics</MenuItem>
+                                </LinkContainer>
+                                <LinkContainer to="#">
+                                    <MenuItem disabled eventKey={5.3}>System Variables</MenuItem>
+                                </LinkContainer>
+                                <LinkContainer to="#">
+                                    <MenuItem disabled eventKey={5.4}>Update Membership List</MenuItem>
+                                </LinkContainer>
+                            </NavDropdown>
+                        </Nav>
+                        <Navbar.Form pullRight>
+                            <LinkContainer to="#">
+                                <Button bsStyle="primary"> Logout </Button>
+                            </LinkContainer>
+                        </Navbar.Form>
+                    </Navbar>
                     <Route exact path="/" component={GearPage} />
                     <Route path="/gear" component={GearPage} />
                     <Route path="/reservation" component={ReservationPage} />
