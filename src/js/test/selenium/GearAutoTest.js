61a80ff103cc5c5092b24aeffa63e3e868a0ba45
Bennett Hreherchuk <hreherch@ualberta.ca>
2018-11-11 16:54:31 -0700

    Adds latest E2E testing to master (#223)

--- src/js/test/selenium/GearAutoTest.js
+++ src/js/test/selenium/GearAutoTest.js
@@ -7,7 +7,7 @@ const createGear = (code, fee, desc, category) => {
     browser.click("[name=gearCategory]");
     browser.selectByVisibleText("[name=gearCategory]", category);
     browser.click("button*=Submit");
-    browser.waitForVisible(`td=${code}`, 3000);
+    browser.waitForVisible(`td=${code}`);
 };
 
 /**
@@ -28,56 +28,57 @@ describe("Basic Gear Management", () => {
     it("Gear-Mgmt-01: Can Create A Category", () => {
         browser.url("/gear");
 
-        browser.waitForVisible("[id=gear-view-tabs-tab-2]", 3000);
+        browser.waitForVisible("[id=gear-view-tabs-tab-2]");
         browser.click("[id=gear-view-tabs-tab-2]");
 
-        browser.waitForVisible("button*=Add New Category", 3000);
+        browser.waitForVisible("button*=Add New Category");
         browser.click("button*=Add New Category");
 
-        browser.waitForVisible("[name=category]", 3000);
-        browser.setValue("[name=category]", "backpack");
+        browser.waitForVisible("[name=category]");
+        browser.setValue("[name=category]", "Backpack");
 
-        browser.waitForVisible("button*=Submit", 3000);
+        browser.waitForVisible("button*=Submit");
         browser.click("button*=Submit");
 
-        browser.waitForVisible("td=backpack", 3000);
-        expect(browser.isExisting("td=backpack"), "there should be a row with the backpack category.").to.be.true;
+        browser.waitForVisible("td=Backpack");
+        expect(browser.isExisting("td=Backpack"), "there should be a row with the backpack category.").to.be.true;
     });
 
     it("Gear-Mgmt-04: Can Create Gear", () => {
         browser.url("/gear");
         browser.click("[id=gear-view-tabs-tab-1]");
 
-        createGear("BP01", "50.00", "Brand new backpack! Red, fits lots.", "backpack");
+        createGear("BP01", "50.00", "Brand new backpack! Red, fits lots.", "Backpack");
 
         expect(browser.isExisting("td=BP01"), "there should be a row with the new gear.").to.be.true;
     });
 
     it("Gear-Mgmt-07: Edit Details on Gear", () => {
-        createGear("BP02", "0.01", "Slighty oldr bckp. Fits some.", "backpack");
+        createGear("BP02", "0.01", "Slighty oldr bckp. Fits some.", "Backpack");
         expect(browser.isExisting("td=BP02"), "there should be a row with the new gear.").to.be.true;
         expect(browser.isExisting("td=Slighty oldr bckp. Fits some."), "there should be a row with the new description.").to.be.true;
         expect(browser.isExisting("td=0.01"), "there should be a row with the small fee.").to.be.true;
 
         // Test that we can open the editor for BP01
         browser.click("[name=edit-gear-BP01]");
-        browser.waitForVisible("[name=gearCode]", 3000);
+        browser.waitForVisible("[name=gearCode]");
         expect(browser.getValue("[name=gearCode]")).to.equal("BP01");
-        browser.waitForVisible("button*=Close");
-        browser.click("button*=Close");
-        browser.waitForVisible(".modal-open", 5000, true);
+        browser.waitForVisible(".close");
+        browser.click(".close");
+        browser.waitForVisible(".modal-open", null, true);
 
         // Open the editor for BP02
         browser.click("[name=edit-gear-BP02]");
-        browser.waitForVisible("[name=gearCode]", 3000);
+        browser.waitForVisible("[name=gearCode]");
         expect(browser.getValue("[name=gearCode]")).to.equal("BP02");
         browser.clearElement("[name=gearDescription]");
         browser.setValue("[name=gearDescription]", "Slightly older blue backpack.");
         browser.clearElement("[name=depositFee]");
         browser.setValue("[name=depositFee]", "49.99");
         browser.click("button*=Submit");
-        browser.waitForVisible("td=49.99", 3000);
-        browser.waitForVisible("td=Slightly older blue backpack.", 3000);
+        browser.waitForVisible(".modal-open", null, true);
+        browser.waitForVisible("td=49.99");
+        browser.waitForVisible("td=Slightly older blue backpack.");
 
         // expect the data we set and removed
         expect(browser.isExisting("td=Slighty oldr bckp. Fits some."), "we edited the description from this.").to.be.false;
@@ -90,7 +91,7 @@ describe("Basic Gear Management", () => {
         browser.click("[name=delete-gear-BP02]");
         browser.waitForVisible("button*=Submit");
         browser.click("button*=Submit");
-        browser.waitForVisible("td=BP02", 3000, true);
+        browser.waitForVisible("td=BP02", null, true);
 
         // ensure deletion
         browser.refresh();
@@ -99,30 +100,30 @@ describe("Basic Gear Management", () => {
     });
 
     it("Gear-Mgmt-03: Can Remove Gear Category.", () => {
-        browser.waitForVisible("[id=gear-view-tabs-tab-2]", 3000);
+        browser.waitForVisible("[id=gear-view-tabs-tab-2]");
         browser.click("[id=gear-view-tabs-tab-2]");
 
         // create a new category
-        browser.waitForVisible("button*=Add New Category", 3000);
+        browser.waitForVisible("button*=Add New Category");
         browser.click("button*=Add New Category");
-        browser.waitForVisible("[name=category]", 3000);
+        browser.waitForVisible("[name=category]");
         browser.setValue("[name=category]", "Baaddd Nemme");
-        browser.waitForVisible("button*=Submit", 3000);
+        browser.waitForVisible("button*=Submit");
         browser.click("button*=Submit");
-        browser.waitForVisible(".modal-open", 5000, true);
+        browser.waitForVisible(".modal-open", null, true);
 
         // See it, then delete it.
-        browser.waitForVisible("td=Baaddd Nemme", 3000);
+        browser.waitForVisible("td=Baaddd Nemme");
         browser.click("[name=delete-category-Baaddd-Nemme]");
-        browser.waitForVisible("button*=Submit", 3000);
+        browser.waitForVisible("button*=Submit");
         browser.click("button*=Submit");
-        browser.waitForVisible("td=Baaddd Nemme", 3000, true);
+        browser.waitForVisible("td=Baaddd Nemme", null, true);
 
         // Ensure deletion isn't just in local state
         browser.refresh();
-        browser.waitForVisible("[id=gear-view-tabs-tab-2]", 3000);
+        browser.waitForVisible("[id=gear-view-tabs-tab-2]");
         browser.click("[id=gear-view-tabs-tab-2]");
-        browser.waitForVisible("td=backpack");
+        browser.waitForVisible("td=Backpack");
         expect(browser.isExisting("td=Baaddd Nemme")).to.be.false;
     });
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -82,7 +82,7 @@ exports.config = {
     baseUrl: "http://localhost:8081",
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 20000,
     //
     // Default timeout in milliseconds for request
     // if Selenium Grid doesn't send response
