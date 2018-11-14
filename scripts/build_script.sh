03252516ca91680fc7586e37d3b21a8f48322241
hreherch <hreherch@ualberta.ca>
2018-11-14 10:50:05 -0700

    endhere

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,18 +1,20 @@
-source outdoorsclubenv/bin/activate
-cd virtual-outdoors-club
-eval "$(ssh-agent -s)"
-ssh-add ../.ssh/deploy_rsa
-git checkout master
-git pull
-pkill -f npm
-pkill -f node
-pkill -f manage.py
-rm -rf ./dist
-mkdir dist
-mv tmp dist
-rm -rf tmp
-pip install -r requirements.txt
-python ./src/django/manage.py runserver 0.0.0.0:8000 &
-python ./src/django/manage.py process_tasks &
-node server.js &
-exit
+ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 'bash -s' << ENDHERE
+    source outdoorsclubenv/bin/activate
+    cd virtual-outdoors-club
+    eval "$(ssh-agent -s)"
+    ssh-add ../.ssh/deploy_rsa
+    git checkout master
+    git pull
+    pkill -f npm
+    pkill -f node
+    pkill -f manage.py
+    rm -rf ./dist
+    mkdir dist
+    mv tmp dist
+    rm -rf tmp
+    pip install -r requirements.txt
+    python ./src/django/manage.py runserver 0.0.0.0:8000 &
+    python ./src/django/manage.py process_tasks &
+    node server.js &
+    exit
+ENDHERE
