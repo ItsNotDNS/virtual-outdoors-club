f735848e2ec179e810e77bb0550d51e94ae7bae7
hreherch <hreherch@ualberta.ca>
2018-11-14 10:31:52 -0700

    remove build, try scp

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -2,14 +2,12 @@ source outdoorsclubenv/bin/activate
 cd virtual-outdoors-club
 eval "$(ssh-agent -s)"
 ssh-add ../.ssh/deploy_rsa
-git pull origin master
 git checkout master
-git pull origin master
+git pull
 pkill -f npm
 pkill -f node
 pkill -f manage.py
 pip install -r requirements.txt
-npm install
 python ./src/django/manage.py runserver 0.0.0.0:8000 &
 python ./src/django/manage.py process_tasks &
 node server.js &
