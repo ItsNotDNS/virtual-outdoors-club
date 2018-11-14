32ad6d2af61f617d120c15f846d321e42be5c532
dong-alex <adong@ualberta.ca>
2018-11-14 13:01:10 -0700

    Adjust scripts

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -14,7 +14,9 @@ mkdir dist
 mv tmp/* dist
 rm -rf tmp
 pip install -r requirements.txt
+echo "node"
 nohup node server.js &
 python ./src/django/manage.py process_tasks &
 python ./src/django/manage.py runserver 0.0.0.0:8000 &
-exit 0
+echo "django"
+exit
