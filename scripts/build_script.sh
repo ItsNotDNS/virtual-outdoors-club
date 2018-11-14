6c889a483af05e218a330dbda56f29e69f691c9c
dong-alex <adong@ualberta.ca>
2018-11-13 23:02:05 -0700

    Update scripts

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,7 +1,10 @@
 source outdoorsclubenv/bin/activate
 cd virtual-outdoors-club
-kill $(lsof -ti :4444)
-kill $(lsof -ti :8000)
-kill $(lsof -ti :8081)
+git pull
+git checkout master
+git pull
+kill $(lsof -t -i :4444)
+kill $(lsof -t -i :8000)
+kill $(lsof -t -i :8081)
 python ./src/django/manage.py runserver 0.0.0.0:8000 &
 python ./src/django/manage.py process_tasks &
