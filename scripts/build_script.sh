f0bc588ef755f215025497e9d839069935491fee
dong-alex <adong@ualberta.ca>
2018-11-14 12:55:46 -0700

    Change script structure

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 'bash -s' <<- "EOF"
-    source outdoorsclubenv/bin/activate
-    cd virtual-outdoors-club
-    pkill -f agent
-    eval "$(ssh-agent -s)"
-    ssh-add ../.ssh/deploy_rsa
-    git checkout ssh-encryption
-    git pull
-    pkill -f npm
-    pkill -f node
-    pkill -f manage.py
-    rm -rf ./dist
-    mkdir dist
-    mv tmp/* dist
-    rm -rf tmp
-    pip install -r requirements.txt
-    nohup node server.js &
-    python ./src/django/manage.py process_tasks &
-    python ./src/django/manage.py runserver 0.0.0.0:8000 &
-    exit 0
-EOF
+source outdoorsclubenv/bin/activate
+cd virtual-outdoors-club
+pkill -f agent
+eval "$(ssh-agent -s)"
+ssh-add ../.ssh/deploy_rsa
+git checkout ssh-encryption
+git pull
+pkill -f npm
+pkill -f node
+pkill -f manage.py
+rm -rf ./dist
+mkdir dist
+mv tmp/* dist
+rm -rf tmp
+pip install -r requirements.txt
+nohup node server.js &
+python ./src/django/manage.py process_tasks &
+python ./src/django/manage.py runserver 0.0.0.0:8000 &
+exit 0
