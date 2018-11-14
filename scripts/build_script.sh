a0195043d7a8b80d7978577e382659e6f63bd198
dong-alex <adong@ualberta.ca>
2018-11-14 12:02:20 -0700

    Add exit code

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -17,5 +17,5 @@ ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 'bash -s' <<- "EOF"
     node server.js &
     python ./src/django/manage.py process_tasks &
     python ./src/django/manage.py runserver 0.0.0.0:8000 &
-    ~.
+    exit 0
 EOF
