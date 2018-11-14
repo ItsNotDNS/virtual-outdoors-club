cae9fe0e4fd90e7189712ad79f98d77d9be5c33d
dong-alex <adong@ualberta.ca>
2018-11-13 23:55:43 -0700

    Adjust git config file and scripts

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,13 +1,13 @@
 source outdoorsclubenv/bin/activate
 cd virtual-outdoors-club
 eval "$(ssh-agent -s)"
-echo "$SSH_AUTH_SOCK"
 ssh-add ../.ssh/deploy_rsa
 git pull origin master
 git checkout master
 git pull origin master
-kill $(lsof -t -i :4444)
-kill $(lsof -t -i :8000)
-kill $(lsof -t -i :8081)
+pkill -f npm
+pkill -f manage.py
+pip install -r requirements.txt
 python ./src/django/manage.py runserver 0.0.0.0:8000 &
 python ./src/django/manage.py process_tasks &
+node server.js &
