5be727536533f3f528b863976a07854f50c4a263
dong-alex <adong@ualberta.ca>
2018-11-14 20:16:36 -0700

    idk

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -16,7 +16,7 @@ ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 <<EOF
     rm -rf tmp
     pip install -r requirements.txt
     python ./src/django/manage.py process_tasks &
-    sudo npm start &
+    node server.js &
     python ./src/django/manage.py runserver 0.0.0.0:8000 &
     exit 0 && exit
 EOF
