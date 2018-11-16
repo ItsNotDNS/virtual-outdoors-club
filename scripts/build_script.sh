09fb95a5257161c829d3b994ef568bb5aff1eaab
dong-alex <adong@ualberta.ca>
2018-11-15 17:57:43 -0700

    shame

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -16,7 +16,7 @@ ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 <<EOF
     rm -rf tmp
     pip install -r requirements.txt
     nohup python ./src/django/manage.py process_tasks &
-    nohup node server.js & disown
+    nohup node server.js &
     nohup python ./src/django/manage.py runserver 0.0.0.0:8000 &
     exit 0 && exit
 EOF
