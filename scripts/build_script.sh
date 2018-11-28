ac186d0fb436f1c6528eeaca1448fd8e6ed55354
dong-alex <adong@ualberta.ca>
2018-11-27 20:09:44 -0700

    Add nginx configuration and scp adjustment

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -2,21 +2,19 @@
 ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 <<EOF
     source outdoorsclubenv/bin/activate
     cd virtual-outdoors-club
-    eval "$(ssh-agent -s)"
-    ssh-add ../.ssh/deploy_rsa
-    git checkout ssh-encryption
+    git checkout master
     git pull
-    ssh-agent -k
     pkill -f npm
     pkill -f node
     pkill -f manage.py
-    rm -rf ./dist
-    mkdir dist
-    mv tmp/* dist
-    rm -rf tmp
     pip install -r requirements.txt
     nohup python ./src/django/manage.py process_tasks &
-    nohup node server.js &
     nohup python ./src/django/manage.py runserver 0.0.0.0:8000 &
+    systemctl restart nginx
     exit 0
 EOF
+
+    # rm -rf ./dist
+    # mkdir dist
+    # mv tmp/* dist
+    # rm -rf tmp
