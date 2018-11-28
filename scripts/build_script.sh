e16fb6c0e721fb0033f16edf19579da2baa2c702
dong-alex <adong@ualberta.ca>
2018-11-28 00:40:54 -0700

    Readd commands and 'exit' ssh shell

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -7,15 +7,16 @@ ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 <<EOF
     ssh-add ~/.ssh/deploy_rsa
     git checkout master
     git pull
-
-    echo "before"
+    exit 0
+    pip install -r requirements.txt
+    python ./src/django/manage.py createworkers --wipe
     python ./src/django/manage.py process_tasks &
-    echo "after"
     python ./src/django/manage.py runserver 0.0.0.0:8000 &
+    sudo systemctl restart nginx
 EOF
 
-# nohup python ./src/django/manage.py createworkers --wipe &
-# sudo systemctl restart nginx
-#     pip install -r requirements.txt
+# nohup  &
+# 
+#     
     # pkill -f npm
     # pkill -f node
