f0de335395cdcfa73dbe277a89f732b92c1b5e8c
dong-alex <adong@ualberta.ca>
2018-11-28 00:12:58 -0700

    Agent works

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -2,22 +2,18 @@
 ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 <<EOF
     source outdoorsclubenv/bin/activate
     cd virtual-outdoors-club
-    eval "$(ssh-agent -s)"
-    ssh-agent
-    ssh-add -l
+    ssh-agent bash
     ssh-add ~/.ssh/deploy_rsa
-    ssh-add -l
     git checkout master
     git pull
     pkill -f manage.py
     pip install -r requirements.txt
-    nohup python ./src/django/manage.py createworkers --wipe &
     nohup python ./src/django/manage.py process_tasks &
     nohup python ./src/django/manage.py runserver 0.0.0.0:8000 &
-    sudo systemctl restart nginx
-    exit 0
+    
 EOF
 
-# 
+# nohup python ./src/django/manage.py createworkers --wipe &
+# sudo systemctl restart nginx
     # pkill -f npm
     # pkill -f node
