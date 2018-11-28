c2b0b81f30aa8e87fe72deb31c0e74e6f96a09d1
dong-alex <adong@ualberta.ca>
2018-11-28 00:20:43 -0700

    checks

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -2,15 +2,16 @@
 ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 <<EOF
     source outdoorsclubenv/bin/activate
     cd virtual-outdoors-club
+    pkill -f manage.py
     ssh-agent bash
     ssh-add ~/.ssh/deploy_rsa
     git checkout master
     git pull
-    pkill -f manage.py
     pip install -r requirements.txt
+    echo "before"
     nohup python ./src/django/manage.py process_tasks &
+    echo "after"
     nohup python ./src/django/manage.py runserver 0.0.0.0:8000 &
-    
 EOF
 
 # nohup python ./src/django/manage.py createworkers --wipe &
