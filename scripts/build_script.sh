a33c73a0f06a87df937e12929ebf19b048936542
dong-alex <adong@ualberta.ca>
2018-11-27 23:55:07 -0700

    Add sudo

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -2,18 +2,19 @@
 ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 <<EOF
     source outdoorsclubenv/bin/activate
     cd virtual-outdoors-club
-    eval "$(ssh-agent)"
-    ssh-add ~/.ssh/deploy_rsa
+    sudo eval "$(ssh-agent)"
+    sudo ssh-add ~/.ssh/deploy_rsa
     git checkout master
     git pull
     pkill -f manage.py
     pip install -r requirements.txt
-    python ./src/django/manage.py process_tasks &
-    python ./src/django/manage.py runserver 0.0.0.0:8000 &
+    nohup python ./src/django/manage.py createworkers --wipe &
+    nohup python ./src/django/manage.py process_tasks &
+    nohup python ./src/django/manage.py runserver 0.0.0.0:8000 &
     sudo systemctl restart nginx
     exit 0
 EOF
 
-# python ./src/django/manage.py createworkers --wipe &
+# 
     # pkill -f npm
     # pkill -f node
