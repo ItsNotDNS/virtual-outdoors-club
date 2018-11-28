61374c2399e97b88f326084bce68f44589271fe6
dong-alex <adong@ualberta.ca>
2018-11-28 00:43:20 -0700

    Bash inception

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 <<EOF
+    ssh-agent bash
+    ssh-add ~/.ssh/deploy_rsa
     source outdoorsclubenv/bin/activate
     cd virtual-outdoors-club
     pkill -f manage.py
-    ssh-agent bash
-    ssh-add ~/.ssh/deploy_rsa
     git checkout master
     git pull
-    exit 0
-    pip install -r requirements.txt
-    python ./src/django/manage.py createworkers --wipe
-    python ./src/django/manage.py process_tasks &
-    python ./src/django/manage.py runserver 0.0.0.0:8000 &
+    pip3 install -r requirements.txt
+    python3 ./src/django/manage.py createworkers --wipe
+    python3 ./src/django/manage.py process_tasks &
+    python3 ./src/django/manage.py runserver 0.0.0.0:8000 &
     sudo systemctl restart nginx
+    exit 0
+    exit 0
 EOF
 
 # nohup  &
