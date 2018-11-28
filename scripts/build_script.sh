661d3efc2b314fc1093545a9d6c977a8c2409e77
dong-alex <adong@ualberta.ca>
2018-11-28 00:37:17 -0700

    remove requirements

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -7,14 +7,15 @@ ssh -i /tmp/key_outdoors.pem ubuntu@199.116.235.142 <<EOF
     ssh-add ~/.ssh/deploy_rsa
     git checkout master
     git pull
-    pip install -r requirements.txt
+
     echo "before"
-    nohup python ./src/django/manage.py process_tasks &
+    python ./src/django/manage.py process_tasks &
     echo "after"
-    nohup python ./src/django/manage.py runserver 0.0.0.0:8000 &
+    python ./src/django/manage.py runserver 0.0.0.0:8000 &
 EOF
 
 # nohup python ./src/django/manage.py createworkers --wipe &
 # sudo systemctl restart nginx
+#     pip install -r requirements.txt
     # pkill -f npm
     # pkill -f node
