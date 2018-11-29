5c547270b45d30689c4ea2e8c5e0d7808355f2e2
Shardul Shah <31705277+shardul-shah@users.noreply.github.com>
2018-11-28 18:50:56 -0700

    Improving system disable output messages (#401)
    Closes #398
    
    Improves and adds output messages
    Fixes voiding reservations bug

--- src/django/api/views/SystemView.py
+++ src/django/api/views/SystemView.py
@@ -9,74 +9,87 @@ from .PayPalView import process
 
 class SystemView(APIView):
 
-    # get request to retrieve the boolean variables of system
-    def get(self, request):
-        if not request.user.is_authenticated:
-            return RespError(401, "You don't have permission to visit this page!")
+	# get request to retrieve the boolean variables of system
+	def get(self, request):
+		if not request.user.is_authenticated:
+			return RespError(401, "You don't have permission to visit this page!")
 
-        sys = System.objects.all()
+		sys = System.objects.all()
 
-        # if a get request for system has been made without the system being initialized, initialize it:
-        if not sys:
-            serviceEntry = System.objects.create(service="disableSys", disabled=False)
-            serviceEntry.save()
-            sys = System.objects.all()
+		# if a get request for system has been made without the system being initialized, initialize it:
+		if not sys:
+			serviceEntry = System.objects.create(service="disableSys", disabled=False)
+			serviceEntry.save()
+			sys = System.objects.all()
 
-        serial = SystemSerializer(sys, many=True)
-        return Response({"data": serial.data}) 
+		serial = SystemSerializer(sys, many=True)
+		return Response({"data": serial.data}) 
 
-    def post(self, request):
-        # function to disable or enable the rental system
+	def post(self, request):
+		# function to disable or enable the rental system
 
-        if not request.user.is_authenticated:
-            return RespError(401, "You don't have permission to visit this page!")
-            
-        request = request.data
+		if not request.user.is_authenticated:
+			return RespError(401, "You don't have permission to visit this page!")
+			
+		request = request.data
 
-        # Common checks to make sure request is valid
+		# Common checks to make sure request is valid
 
-        if "disableSys" not in request:
-            return RespError(400, "You are missing the required key 'disableSys' in the request.")
+		if "disableSys" not in request:
+			return RespError(400, "You are missing the required key 'disableSys' in the request.")
 
-        # Set the boolean of the disableSys service in the system to the one passed in by the request
-        # If the service doesn't already exist, create it with the boolean in the request
-        try:
-            serviceEntry = System.objects.get(service="disableSys")
-            serviceEntry.disabled = request["disableSys"]
-            serviceEntry.save()
+		# Set the boolean of the disableSys service in the system to the one passed in by the request
+		# If the service doesn't already exist, create it with the boolean in the request
+		try:
+			serviceEntry = System.objects.get(service="disableSys")
+			serviceEntry.disabled = request["disableSys"]
+			serviceEntry.save()
 
-        except System.DoesNotExist:
-            serviceEntry = System.objects.create(service="disableSys", disabled=request["disableSys"])
-            serviceEntry.save()
+		except System.DoesNotExist:
+			serviceEntry = System.objects.create(service="disableSys", disabled=request["disableSys"])
+			serviceEntry.save()
 
-        disableSysService = System.objects.get(service="disableSys")
-        disableSysBool = disableSysService.disabled
+		disableSysService = System.objects.get(service="disableSys")
+		disableSysBool = disableSysService.disabled
 
-        if "cancelRes" in request:
-            if not disableSysBool:
-                return RespError(400, "You cannot cancel any reservations while enabling the system.")
+		if "cancelRes" in request:
+			if not disableSysBool:
+				return RespError(400, "You cannot cancel any reservations while enabling the system.")
 
-            cancelResBool = request["cancelRes"]
-            # Cancels all reservations with status = REQUESTED, APPROVED, and
-            # PAID if cancelRes == True in the system (and the rental service is being disabled)
-            if disableSysBool and cancelResBool:
-                allResToCancel = Reservation.objects.filter(Q(status="REQUESTED") | Q(status="APPROVED") | Q(status="PAID"))
+			cancelResBool = request["cancelRes"]
+			# Cancels all reservations with status = REQUESTED, APPROVED, and
+			# PAID if cancelRes == True in the system (and the rental service is being disabled)
+			if disableSysBool and cancelResBool:
+				allResToCancel = Reservation.objects.filter(Q(status="REQUESTED") | Q(status="APPROVED") | Q(status="PAID"))
 
-                # voids any paid reservations
-                unvoidedRes = []
-                paidRes = Reservation.objects.filter(status="PAID")
+				# voids any paid reservations
+				unvoidedRes = []
+				paidRes = Reservation.objects.filter(status="PAID")
 
-                for res in paidRes:
-                    processReturn = process(res, 0) 
-                    if not processReturn: 
-                        unvoidedRes.append(res.id)
+				for res in paidRes:
+					processReturn = process(res, 0) 
+					if processReturn: 
+						# if process() returns an error message, track all unvoided reservations
+						unvoidedRes.append(res.id)
 
-                allResToCancel = allResToCancel.exclude(id__in=unvoidedRes)
+				# sends cancellation emails accordingly
+				cancelled(allResToCancel)
 
-                # sends cancellation emails accordingly
-                cancelled(allResToCancel)
+				outputStr=""
+				for resId in unvoidedRes:
+					outputStr+=str(resId)
+					if resId != unvoidedRes[-1]:
+						outputStr+=", "
 
-                # cancels all appropriate reservations 
-                allResToCancel.update(status="CANCELLED")
+				# cancels all appropriate reservations 
+				allResToCancel.update(status="CANCELLED")
 
-        return Response(200)    
+				# if there are any unvoided reservations, output a specific error message
+				if unvoidedRes:
+					return Response({"message": "All reservations not checked out were succesfully cancelled, but" +  
+						"the following paid reservations could not be voided right now: reservation IDs " +
+						outputStr + "."}, 200)
+				else:
+					return Response({"message": "All reservations not checked out were succesfully cancelled."}, 200)
+
+		return Response(200)    
